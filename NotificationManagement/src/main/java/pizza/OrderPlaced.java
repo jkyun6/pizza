ecebf67571206dc59cb745d708c146b700e0900b
newmembrane <jonghun283@naver.com>
2020-10-27 13:54:14 +0900

    .

--- NotificationManagement/src/main/java/pizza/OrderPlaced.java
+++ NotificationManagement/src/main/java/pizza/OrderPlaced.java
@@ -5,35 +5,50 @@ public class OrderPlaced extends AbstractEvent {
 
     private Long id;
     private Long customerId;
-    private Integer totalPrice;
+    private String state;
+    private Integer price;
     private String paymentMethod;
+    private String menuOption;
+    private String notificationType;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
+
     public Long getCustomerId() {
         return customerId;
     }
-
     public void setCustomerId(Long customerId) {
         this.customerId = customerId;
     }
-    public Integer getTotalPrice() {
-        return totalPrice;
+
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
     }
 
-    public void setTotalPrice(Integer totalPrice) {
-        this.totalPrice = totalPrice;
-    }
     public String getPaymentMethod() {
         return paymentMethod;
     }
-
     public void setPaymentMethod(String paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
+
+    public String getMenuOption() {
+        return menuOption;
+    }
+    public void setMenuOption(String menuOption) {
+        this.menuOption = menuOption;
+    }
+
+    public String getNotificationType() { return notificationType; }
+    public void setNotificationType(String notificationType) { this.notificationType = notificationType; }
 }
